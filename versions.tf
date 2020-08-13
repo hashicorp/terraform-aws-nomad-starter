583c075aafd22de5ac61f45e7de51634613742b5
Omar Khawaja <khawaja910@gmail.com>
2020-08-13 13:56:57 -0400

    upgrade syntax for TF 0.13+ and pin aws and random provider versions

--- versions.tf
+++ versions.tf
@@ -1,4 +1,14 @@
 # names required TF version
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "2.70.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "2.3.0"
+    }
+  }
 }
